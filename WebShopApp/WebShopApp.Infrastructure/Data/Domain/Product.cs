9072dfd4177e22932240b0097f4157caa89c6b55
“AlexKirilov7” <a.v.kirilov2020@pmg-pernik.com>
2024-12-12 11:33:08 +0200

    asd

--- WebShopApp/WebShopApp.Infrastructure/Data/Domain/Product.cs
+++ WebShopApp/WebShopApp.Infrastructure/Data/Domain/Product.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WebShopApp.Infrastructure.Data.Domain
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string ProductName { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Brand))]
+        public virtual Brand Brand { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; } = null!;
+
+        public string Picture { get; set; } = null!;
+
+        [Range(0, 5000)]
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+
+        public virtual IEnumerable<Order> Orders { get; set; } = new List<Order>();
     }
 }
