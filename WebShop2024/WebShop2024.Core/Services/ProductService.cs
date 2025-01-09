3d4b122bad45aacb247aa77dec6f2beb1eeed53d
“AlexKirilov7” <a.v.kirilov2020@pmg-pernik.com>
2025-01-09 10:35:51 +0200

    asd

--- WebShop2024/WebShop2024.Core/Services/ProductService.cs
+++ WebShop2024/WebShop2024.Core/Services/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,6 +34,69 @@ namespace WebShop2024.Core.Services
 
             _context.Products.Add(item);
             return _context.SaveChanges() != 0; 
+
+
+        }
+
+        public Product GetProductById (int productId) 
+        {
+            return _context.Products.Find(productId);
+        }
+
+        public List<Product> GetProducts()
+        {
+            List<Product> products = _context.Products.ToList();
+            return products;
+        }
+
+        public List<Product> GetProducts(string searchStringCategoryName, string searchStringBrandName) 
+        {
+            List<Product> products = _context.Products.ToList();
+
+            if(!String.IsNullOrEmpty(searchStringCategoryName) && !String.IsNullOrEmpty(searchStringBrandName)) 
+            {
+                products = products.Where(x => x.Category.CategoryName.ToLower().Contains
+                (searchStringCategoryName.ToLower())
+                && x.Brand.BrandName.ToLower().Contains(searchStringBrandName.ToLower())).ToList(); 
+            }
+            else if(!String.IsNullOrEmpty(searchStringCategoryName))
+            {
+                products = products.Where(x => x.Category.CategoryName.ToLower().Contains(searchStringCategoryName.ToLower())).ToList() ;
+            }
+            else if(!String.IsNullOrEmpty (searchStringBrandName)) 
+            {
+                products = products.Where(x => x.Brand.BrandName.ToLower().Contains(searchStringBrandName.ToLower())).ToList();
+            }
+            return products;
+        }
+        public bool RemoveById(int productId)
+        {
+            var product = GetProductById(productId);
+            if(product == default(Product)) 
+            {
+                return false;
+            }
+            _context.Remove(product);
+            return _context.SaveChanges() !=0;
+        }
+
+        public bool Update(int productId, string name, int brandId, int categoryId, string picture, int quantity, decimal price, decimal discount)
+        {
+            var product = GetProductById(productId);
+            if(product == default(Product))
+            {
+                return false;
+            }
+            product.ProductName = name;
+
+            product.Brand = _context.Brands.Find(brandId);
+            product.Category = _context.Categories.Find(categoryId);
+            product.Picture = picture;
+            product.Quantity = quantity;
+            product.Price = price;
+            product.Discount = discount;
+            _context.Update(product);
+            return _context.SaveChanges() != 0;
         }
     }
 }
